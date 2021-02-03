227c04fa1c245b656f9c1c5d71464e9be8a2fa15
Randal Gardiner <randal.gardiner@mynbps.org>
2021-02-03 02:30:56 +0000

    I figured out why my code was not compiling. This code is from my previous project which I copied and pasted over into this new project.

--- App.java
+++ App.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+import java.util.Random;
+
 class App {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+         int computerNumber;
+         int userNumber = 0;
+         
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+
+        computerNumber = rand.nextInt(101);
+        boolean win = false;
+
+         while(userNumber != computerNumber){
+        System.out.print("Guess a number between 1 and 100: ");
+        userNumber = scan.nextInt();
+      
+        if(userNumber == computerNumber){
+        win = true;
+        System.out.println("Correct!");
+        }
+        
+        else if(userNumber > computerNumber){
+        System.out.println("The number you have guessed is too high!"); 
+        }
+        
+        else if(userNumber < computerNumber){
+        System.out.println("The number you have guessed is too low!");
+        }
+        }
+    
   }
 }
